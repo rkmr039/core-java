9ed0623ceeee39a45ad0444224476b6da778e289
rkmr039 <rkmr039@gmail.com>
2019-09-19 09:39:00 +0530

    .

---
+++ Day13/InventoryProjectServlet/src/com/hcl/inventory/Stock.java
@@ -0,0 +1,47 @@
+package com.hcl.inventory;
+
+public class Stock {
+  
+  private String stockId;
+  private String itemName;
+  private double price;
+  private int quantityAvail;
+  
+  public String getStockId() {
+    return stockId;
+  }
+  
+  public void setStockId(String stockId) {
+    this.stockId = stockId;
+  }
+  
+  public String getItemName() {
+    return itemName;
+  }
+  
+  public void setItemName(String itemName) {
+    this.itemName = itemName;
+  }
+  
+  public double getPrice() {
+    return price;
+  }
+  
+  public void setPrice(double price) {
+    this.price = price;
+  }
+  
+  public int getQuantityAvail() {
+    return quantityAvail;
+  }
+  
+  public void setQuantityAvail(int quantityAvail) {
+    this.quantityAvail = quantityAvail;
+  }
+  
+  @Override
+  public String toString() {
+    return "Stock [stockId=" + stockId + ", itemName=" + itemName + ", price=" + price + ","
+         + " quantityAvail=" + quantityAvail + "]";
+  }
+}
